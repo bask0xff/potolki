9aa69524928eaa3821f06c0d8ea6fdc1d754e184
sergey <bask0xff@gmail.com>
2026-09-03 06:05:06 +0200

    update configs for first launch

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,8 @@ plugins {
 android {
     namespace = "com.bask0xff.potolki_mobile"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    /*ndkVersion = flutter.ndkVersion*/
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
